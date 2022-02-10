e735b0367abcd67ff9822ad608f2e72560d82722
TeofilGaming <teofilfer11@gmail.com>
2022-02-10 14:19:19 +0100

    Zrobione

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,11 +10,14 @@
 <body>
     <div type="text-effect"><h2>Strona o piłce nożnej</h2></div>       
     <div id="lewa"><img src="strona.png" ></div>
-    <div id="prawa"><p><?php
-function liga(){
+    <div id="prawa"><p><?PHP function liga(){
 	$db = mysqli_connect("localhost", "root", "", "liga_database");
     $x = "SELECT * FROM liga";
-?></p></div>
+	$result = mysqli_query($db, $x);
+    while ($row = mysqli_fetch_array($result)) {
+        print("Nazwa:".$row['nazwa_ligi'] ." "."Kraj pochodzenia:". $row['kraj'] ." "."liczba graczy:". $row['Liczba_druzyn']."<br>");}
+};
+    liga()?></p></div>
     <div id="stopka"><a href="https://www.facebook.com/profile.php?id=100028407642525"> <img src="ja.png"></a>Tymofii Ferenets 2pT grupa: hiszpańska</div>
 </body>
 </html>
