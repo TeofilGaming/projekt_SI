815d0881c8c330593d96d2ff1939bdddaf5bd601
TeofilGaming <teofilfer11@gmail.com>
2022-02-23 19:31:59 +0100

    Robienia slajdera

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
     <div type="text-effect"><h2>Strona o piłce nożnej</h2></div>       
     <div id="lewa">
         <div id="ramka">
-            <img src="strona.png" >
+            <img src="liga1.png" >
             <div id="slajdy">
-                <img id="slide1" src="strona.png" alt="przykladowa grafika" />
-                <img id="slide2" src="xd.png" alt="przykladowa grafika" />
-                <img id="slide3" src="ja.png" alt="przykladowa grafika" />
-                <img id="slide4" src="ghoul_sss_rank.jpg" alt="przykladowa grafika" />
+                <img id="slide1" src="liga1.png" alt="przykladowa grafika" />
+                <img id="slide2" src="liga2.png" alt="przykladowa grafika" />
+                <img id="slide3" src="liga3.png" alt="przykladowa grafika" />
+                <img id="slide4" src="liga4.png" alt="przykladowa grafika" />
             </div>
         </div>
         <div id="nawigacja">
@@ -26,14 +26,16 @@
             <a href="#slide4">4</a>
         </div>
     </div>
-    <div id="prawa"><p><?PHP function liga(){
+    <div id="prawa"><p><?PHP
 	$db = mysqli_connect("localhost", "root", "", "liga_database");
     $x = "SELECT * FROM liga";
 	$result = mysqli_query($db, $x);
     while ($row = mysqli_fetch_array($result)) {
         print("Nazwa:".$row['nazwa_ligi'] ." "."Kraj pochodzenia:". $row['kraj'] ." "."liczba graczy:". $row['liczba_druzyn']."<br>");}
-};
-    liga()?></p></div>
+    mysqli_close($db);
+
+
+    ?></p></div>
     <div id="stopka"><a href="https://www.facebook.com/profile.php?id=100028407642525"> <img src="ja.png"></a>Tymofii Ferenets 2pT grupa: hiszpańska</div>
 </body>
 </html>
